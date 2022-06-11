3b783435ea6b31c20ca58d903d0d46333dd7ed1c
riteshpoudel201 <39463468+riteshpoudel201@users.noreply.github.com>
2022-06-11 13:40:18 +0545

    Initial commit

---
+++ loginprocess.php
@@ -0,0 +1,25 @@
+<?php 
+$conn = new mysqli("localhost","root","","login");
+if(isset($_POST["submit"])){
+    $user = $_POST["username"];
+    $pass = $_POST["password"];
+
+    $sql = "SELECT * FROM user WHERE username='".$user."' AND password='$pass'";
+    $result = mysqli_query($conn,$sql);
+    if($result){
+    $row_num = mysqli_num_rows($result);
+    if($row_num > 0){
+        echo "You have successfully logged in.";
+    }
+    else{
+        echo "You are not logged in";
+    }
+}
+else{
+    echo "Invalid input";
+}
+}
+else{
+    echo "Not Working";
+}
+?>
